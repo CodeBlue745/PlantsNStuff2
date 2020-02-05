7de183748e4b0a35e34ec210f506b049f3aaeb39
Stephen Robbins <10696812@uvu.edu>
2020-02-05 16:56:03 -0700

    Added a base and derived class to backend and creted a GUI.

---
+++ PlantsNStuffSDR/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantsNStuffSDR
+{
+    /// <summary>
+    /// This public class inherits the Plant class.
+
+    /// </summary>
+    public class Tree : Plant
+    {
+        /// <summary>
+        /// We need a name, environment and type or name, env and type variables.
+        /// The base class represents everything from the Plant class.
+        /// </summary>
+        public Tree() : base("","","")
+        {
+
+        }
+        public string BranchType { get; set;}
+    }
+}
